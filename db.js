47405f7619c9d83a4e5fe7f531902450550fcc0e
KartanonKanye <rruuskanen12@gmail.com>
2023-05-13 15:53:48 +0300

    refactored the connecting to db part from index to a new file db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+//dotenv to accees environment variables defined in the .env file.
+require('dotenv').config({path: "./.env"})
+const {Sequelize} = require('sequelize')
+
+//define database parameters and connect to the database
+const sequelize = new Sequelize(
+    {database: 'digitalfuksi', 
+    username: 'postgres', 
+    password: process.env.DATABASE_PASSWORD, 
+    dialect: 'postgres',
+    port: '3049'
+    })
+
+module.exports = sequelize
--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-//dotenv to accees environment variables defined in the .env file. 
 // Express library for the backend. Docs -> https://expressjs.com/
 // I used the sequelize library to work with my postgres database. Docs -> https://sequelize.org/docs/v6/getting-started/
-require('dotenv').config({path: "./.env"})
 const express = require('express')
 const app = express()
-const {Sequelize} = require('sequelize')
+const sequelize = require('./db.js')
 
-// Set up the database parameters
-const sequelize = new Sequelize({database: 'digitalfuksi', 
-                              username: 'postgres', 
-                              password: process.env.DATABASE_PASSWORD, 
-                              dialect: 'postgres',
-                              port: '3049'
-                            })
-
-// Function to connect to the database. 
+// Function to confirm that we have succesfully connected. 
 const main = async () =>{
   try {
     await sequelize.authenticate();
@@ -24,7 +14,7 @@ const main = async () =>{
   }
 }
 
-// when our site receives a request, we return hello world
+// respond to a get request with hello world
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
